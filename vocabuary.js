05621fd1c73508cf6da238bf4e3a37a1407684bb
patharanordev <patharanor.dev@gmail.com>
2016-05-09 16:00:32 +0700

    Create vocabuary.js

---
+++ vocabuary.js
@@ -0,0 +1,20 @@
+
+var questionList = [
+	{question:'absorb', answer:'ซึมซับ'},
+	{question:'abuse', answer:'การละเมิด'},
+	{question:'adjacent', answer:'ติดกัน'},
+	{question:'adjustment', answer:'การปรับ'},
+	{question:'admiring', answer:'ที่เลื่อมใส'},
+	{question:'admixture', answer:'ส่วนผสม'},
+	{question:'adulteretion', answer:'การเจือปน'},
+	{question:'aerosol', answer:'ละอองในอากาศ'},
+	{question:'aggravate', answer:'ซ้ำเติม'},
+	{question:'aggressive', answer:'ก้าวร้าว'},
+	{question:'allusion', answer:'การพาดพิง'},
+	{question:'ambitious', answer:'ทะเยอทะยาน'},
+	{question:'amend', answer:'แก้ไข'},
+	{question:'analyze', answer:'วิเคราะห์'},
+	{question:'announce', answer:'ประกาศ'},
+	{question:'anthropogenic', answer:'กิจกรรมของมนุษย์'},
+	{question:'anticipating', answer:'ที่คาดการณ์ไว้'}
+];
